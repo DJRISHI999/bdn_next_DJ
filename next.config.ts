bd84f546a4d6425506b3d0e0fac026ba13d30331
Dhruv Kumar Jaiswal <80301076+DJRISHI999@users.noreply.github.com>
2025-04-28 08:58:32 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,31 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const nextConfig: NextConfig = {
   compress: true,
-    eslint: {
-        ignoreDuringBuilds: true
-    },
-    output: "standalone",
-    typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-        ignoreBuildErrors: true // TODO: Need to fix the build issue. Run build by removing this line to see actual error to fix.
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: "standalone",
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true, // TODO: Need to fix the build issue. Run build by removing this line to see actual error to fix.
+  },
+  env: {
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+  },
 };
 
 export default nextConfig;
