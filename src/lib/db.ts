305dba3c809c2bfb156d7ae15f1574a91b4c84f5
Dhruv Kumar Jaiswal <80301076+DJRISHI999@users.noreply.github.com>
2025-04-28 08:57:21 +0530

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,18 +1,25 @@
 import mongoose from "mongoose";
+
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
   throw new Error("Please define the MONGO_URI environment variable in your .env file");
 }
 
+let isConnected = 0; // Track the connection state
+
 export const connectToDatabase = async () => {
-  if (mongoose.connection.readyState === 0) {
-    try {
-      await mongoose.connect(MONGO_URI); // No need for useNewUrlParser or useUnifiedTopology
-      console.log("Connected to MongoDB");
-    } catch (error) {
-      console.error("Error connecting to MongoDB:", error);
-      throw new Error("Failed to connect to MongoDB");
-    }
+  if (isConnected) {
+    console.log("Using existing MongoDB connection");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(MONGO_URI);
+    isConnected = db.connections[0].readyState; // 1 means connected
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw new Error("Failed to connect to MongoDB");
   }
 };
